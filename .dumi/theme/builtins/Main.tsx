219733375546a8781e724186dcb7df119cf1231d
lzt <1162300237@qq.com>
2025-03-27 02:13:51 +0800

    feat: update index

---
+++ .dumi/theme/builtins/Main.tsx
@@ -0,0 +1,81 @@
+import { useNavigate, usePrefersColor } from 'dumi';
+import { FC } from 'react';
+import { MapFactory } from 'react-mapboxgl-zt';
+import './main.scss';
+const MapInstance = MapFactory({
+  accessToken:
+    'pk.eyJ1IjoiMnBpYyIsImEiOiJjbTczaXFxbGwwMGNtMm9zYjhjcW1hd3ZkIn0.YCpsYVQawB15Cf5Zyljscg',
+});
+interface Props {
+  title?: string;
+  desc?: string;
+}
+
+const Main: FC<Props> = (props) => {
+  const navigate = useNavigate();
+  const {
+    title = 'React-Mapboxgl-Zt',
+    desc = '一个基于React和MapboxGl的地图组件库',
+  } = props;
+  const [color] = usePrefersColor();
+  console.log(color);
+  return (
+    <div className="main">
+      <div className="content">
+        <div
+          className="title"
+          style={{
+            color: color === 'dark' ? '#000' : '#fff',
+          }}
+        >
+          {title}
+        </div>
+        <div
+          className="desc"
+          style={{
+            color: color === 'dark' ? '#000' : '#fff',
+          }}
+        >
+          {' '}
+          {desc}
+        </div>
+
+        <div
+          className="map"
+          style={{ backgroundColor: color === 'dark' ? '#ccc' : '#fff' }}
+        >
+          <MapInstance></MapInstance>
+        </div>
+
+        <button
+          type="button"
+          style={{
+            color: color === 'dark' ? '#fff' : '#000',
+            background: color === 'dark' ? '#000' : '#fff',
+          }}
+          onClick={() => navigate('/base/start')}
+        >
+          Get Started
+        </button>
+      </div>
+
+      <div className="feature">
+        <div className="feature-item">
+          <img src="/react-mapboxgl-zt/imgs/react.svg" alt="" />
+          <span>用于构建 Web 和原生交互界面的库</span>
+        </div>
+        <div className="feature-item">
+          <img src="/react-mapboxgl-zt/imgs/map.svg" alt="" />
+          <span>用于提供地图数据和地图渲染</span>
+        </div>
+
+        <div className="feature-item">
+          <img src="/react-mapboxgl-zt/imgs/fast.svg" alt="" />
+          <span>帮助你快速上手开发地图应用</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
--- docs/index.md
+++ docs/index.md
@@ -1,20 +1,22 @@
 ---
-hero:
-  title: React-Mapboxgl-Zt
-  description: 一个基于React和MapboxGl的地图组件库。
-  actions:
-    - text: 快速开始
-      link: /base/start
-    - text: 组件
-      link: /components/map
-features:
-  - title: React
-    emoji: ⚛️
-    description: 一个javaScript库，用于构建用户界面的声明式组件。
-  - title: Mapbox
-    emoji: 🗺️
-    description: 一个web地图服务，提供地图数据和地图渲染。
-  - title: React-Mapboxgl-Zt
-    emoji: 🚀
-    description: 一个的地图组件库，帮助你快速上手开发地图应用。
+# hero:
+#   title: React-Mapboxgl-Zt
+#   description: 一个基于React和MapboxGl的地图组件库。
+#   actions:
+#     - text: 快速开始
+#       link: /base/start
+#     - text: 组件
+#       link: /components/map
+# features:
+#   - title: React
+#     emoji: ⚛️
+#     description: 一个javaScript库，用于构建用户界面的声明式组件。
+#   - title: Mapbox
+#     emoji: 🗺️
+#     description: 一个web地图服务，提供地图数据和地图渲染。
+#   - title: React-Mapboxgl-Zt
+#     emoji: 🚀
+#     description: 一个的地图组件库，帮助你快速上手开发地图应用。
 ---
+
+<Main></Main>
